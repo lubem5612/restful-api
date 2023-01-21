b45359a4d6f3bdb8fd8fe4b105ad9c5f883aca69
admin <enginlubem@ymail.com>
2023-01-21 18:45:07 +0100

    change in config

--- config/restfulapi.php
+++ config/restfulapi.php
@@ -23,27 +23,11 @@ return [
                 'pattern' => 'DESC',
             ]
         ],
-        'bookings' => [
-            'model' => \App\Models\Booking::class,
-            'rules' => [
-                'store' => [
-                    'email' => 'required|email',
-                    'password' => 'required|min:6',
-                ],
-                'update' => [
-                    'name' => 'sometimes|string|max:60'
-                ]
-            ],
-            'order' => [
-                'column' => 'created_at',
-                'pattern' => 'DESC',
-            ]
-        ],
     ],
 
     /*
      |
-     | Add prefix to routes restful routes controlled by the Slait Restful package. For example, if the route prefix is
+     | Add prefix to routes restful routes controlled by the Slait Restful package. For example, if the route prefix
      | is 'admin', then the routes in this package will be /api/admin/{endpoint}, /api/admin/{endpoint}/{id} etc. the
      | endpoint variable is defined in the routes array above. e.g /api/admin/users/1, /api/admin/users. You can find more
      | about the routes by running the command 'php artisan route:list'
